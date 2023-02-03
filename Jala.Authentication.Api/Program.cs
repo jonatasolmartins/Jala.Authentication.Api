607806e71ff75c9cfd54b1533037bfbde0895d35
Jonatas Olziris Martins <Jonatas.Martins@highjump.com>
2023-02-03 00:53:42 -0300

    Added comments.

--- Jala.Authentication.Api/Program.cs
+++ Jala.Authentication.Api/Program.cs
@@ -1,4 +1,6 @@
+using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,17 +11,66 @@ var builder = WebApplication.CreateBuilder(args);
 // builder.Services.AddScoped<AuthService>();
 #endregion
 
-const string authScheme = "cookie";
-//We specifies that we want to use authentication with the cookie scheme
-builder.Services.AddAuthentication(authScheme)
-    .AddCookie(authScheme);
+//OAuth is mean to be used to authorization and delegation
+const string cookieAuthScheme = "cookie";
+const string githubScheme = "github";
 
+//We specifies that we want to use authentication with the cookie scheme
+builder.Services.AddAuthentication(cookieAuthScheme)
+    .AddCookie(cookieAuthScheme)
+    .AddOAuth(githubScheme, o =>
+    {
+        /*
+         We specifies that we want to use the cookie scheme to sign in the user.
+         After the claims principal is created with the claims from the github user, we create a cookie with that claims principal information.
+        */
+        o.SignInScheme = cookieAuthScheme;
+        //The client id and secret are the ones we get from the github oauth configuration.
+        o.ClientId = "";
+        o.ClientSecret = "";
+        //The endpoint that will be used to get the authorization code
+        o.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+        //The endpoint that will be used to get the access token
+        o.TokenEndpoint = "https://github.com/login/oauth/access_token";
+        //The path that will be used from the github oauth to call back to our application
+        o.CallbackPath = "/oauth/github/callback";
+        //We specifies that we want to save the tokens returned from the github alongside with the claims principal
+        o.SaveTokens = true;
+        //The endpoint that will be used to get the user information
+        o.UserInformationEndpoint = "https://api.github.com/user";
+        
+        //Map the claims from the github user to the claims principal
+        o.ClaimActions.MapJsonKey("sub", "id");
+        o.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+        //We subscribe to the OnCreatingTicket event to get the user information from the github api
+        o.Events.OnCreatingTicket = async ctx =>
+        {
+            /*
+             * Here is the place where we store the token in the database so we can re-fresh the token when it is about to expire
+             * till the duration of our authentication policy, otherwise the user can end up with a valid cookie/jwt holding an invalid
+             * access token which may cause problems when they try to use it.
+             * Example: myDatabase = ctx.HttpContext.RequestServices.GetRequiredService<MyDatabase>();
+             */
+            
+            using var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //This header is required to get the user information from the github api
+            //AccessTon is just a random string generated from github ans send to our application
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+            //BackChannel is used to call identity provider in a secure way far from the client 
+            using var response = await ctx.Backchannel.SendAsync(request);
+            var user = await response.Content.ReadFromJsonAsync<JsonElement>();
+            //RunClaimActions will map the json data(properties) that we received fom github to the ClaimActions we specify above
+            ctx.RunClaimActions(user);
+        };
+    });
 // Create a authorization rules that will be apply for any request that tries to access an endpoint marked with this policy
 builder.Services.AddAuthorization(builderOptions =>
 {
     //The name of the policy 
     builderOptions.AddPolicy("premium user", pb =>
     {
+        pb.AuthenticationSchemes.Add(cookieAuthScheme);
         //This rule set that the user must be authenticated first them can access the endpoint
         pb.RequireAuthenticatedUser()
             //The user must have the user claim with the value of premium
@@ -105,19 +156,20 @@ app.MapGet("/user", (HttpContext ctx) =>
 }).RequireAuthorization(); //No special policy needed
 
 //To access this endpoint the user need be authenticated with cookie scheme and has the user claim along with the premium respective value
-app.MapGet("/login", (HttpContext ctx /*AuthService authService*/) =>
-{
-    //authService.SignIn();
-    var claim = new List<Claim>
-    {
-        new Claim("user", "normal")
-    };
-    var identity = new ClaimsIdentity(claim, authScheme);
-    var principal = new ClaimsPrincipal(identity);
-    
-    ctx.SignInAsync("cookie", principal);
-    return "User logged in!";
-}).AllowAnonymous();
+// app.MapGet("/login", (HttpContext ctx /*AuthService authService*/) =>
+// {
+//     //authService.SignIn();
+//     var claim = new List<Claim>
+//     {
+//         new Claim("user", "normal"),
+//         new Claim("user_id", Guid.NewGuid().ToString())
+//     };
+//     var identity = new ClaimsIdentity(claim, cookieAuthScheme);
+//     var principal = new ClaimsPrincipal(identity);
+//     
+//     ctx.SignInAsync("cookie", principal);
+//     return "User logged in!";
+// }).AllowAnonymous();
 
 //Call this endpoint to add the premium claim to the user
 //User must be authenticated to access this endpoint
@@ -125,13 +177,35 @@ app.MapGet("/addnewclaim", (HttpContext ctx) =>
 {
     var principal = ctx.User.Clone();
     var claim = new List<Claim> {new Claim("user", "premium")};
-    principal.AddIdentity(new ClaimsIdentity(claim, authScheme));
-    ctx.SignInAsync("cookie", principal);
-    return "User promoted to premium!";
+    principal.AddIdentity(new ClaimsIdentity(claim, cookieAuthScheme));
+    ctx.SignInAsync(cookieAuthScheme, principal);
+    return "Claim added!";
 }).RequireAuthorization();
-
 #endregion
 
+// app.MapGet("/login", () => Results.SignIn(
+//     new ClaimsPrincipal(
+//         new ClaimsIdentity(
+//             new List<Claim>
+//             {
+//                 new Claim("user", "normal"),
+//                 new Claim("user_id", Guid.NewGuid().ToString())
+//             }
+//             , cookieAuthScheme))
+// )).AllowAnonymous();
+
+app.MapGet("/login", () =>
+{
+    return Results.Challenge(new AuthenticationProperties()
+    {
+        RedirectUri = "https://localhost:7090/"
+    } ,authenticationSchemes: new List<string>() { githubScheme });
+}).AllowAnonymous();
+
+app.MapGet("/", (HttpContext ctx) =>
+{
+    return ctx.User.Claims.Select(x => new { x.Type, x.Value}).ToList();
+});
 
 app.Run();
 
